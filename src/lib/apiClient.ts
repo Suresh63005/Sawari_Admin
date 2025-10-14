31e43b50f99b371456f3c3d90cad8f2fc6ef4ed9
diddiakhil <akhil.diddi@innoitlabs.com>
2025-10-14 12:14:58 +0530

    changes

--- src/lib/apiClient.ts
+++ src/lib/apiClient.ts
@@ -9,6 +9,7 @@ import { getToken } from '@/lib/getToken';
 const apiClient: AxiosInstance = axios.create({
   // baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:4445/api",
   baseURL: process.env.NEXT_PUBLIC_API_URL || "https://sawari-api.innoitlabs.net/api",
+  // baseURL: process.env.NEXT_PUBLIC_API_URL || "https://sawari-server.vercel.app/api",
   timeout: 30000,
   headers: {
     'Content-Type': 'application/json',
